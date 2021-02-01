0c1eb8ba297ac7c49b6e1b1ee8abd50fae1f6101
Kate <unicornmagic6111@gmail.com>
2021-02-01 13:00:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//const answer = prompt("Есть 18?", "18");
+//console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
