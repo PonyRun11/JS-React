026b31df80d93704a499c35bef6018b12c65e812
Kate <unicornmagic6111@gmail.com>
2021-02-02 22:07:02 +0300

    Completed the second task. Added loop and condition

--- js/script.js
+++ js/script.js
@@ -3,10 +3,54 @@
 //const answer = prompt("Есть 18?", "18");
 //console.log(answer);
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше фамилия?', '');
-answers[2] = prompt('Сколько Вам лет?', '');
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
 
-console.log(answers);
+// console.log(answers);
+
+
+// // условия 
+// const num = 50;
+// if (num < 49) {
+//    console.log('Err');
+//    } else if (num > 100) {
+//       console.log('Много');
+//    } else {
+//       console.log('OK');
+//    }
+
+// (num===50) ? console.log('OK') : console.log('Err'); // укороченная запись с помощью тернарного оператора ?
+
+// switch(num) {
+//    case 49:
+//       console.log('No');
+//       break;
+//    case 50:
+//       console.log('yeas');
+//       break;
+//    default:
+//       console.log('No'); 
+//       break;
+// }
+
+// ЦИКЛЫ
+let num = 50;
+
+// while (num < 55) {
+//    console.log(num);
+//    num++;
+// }
+
+do {
+   console.log(num);
+   num++;
+}
+while (num <= 55);
+
+for (let i = 1; i < 8; i++) {
+   console.log(num);
+   num++;
+}
