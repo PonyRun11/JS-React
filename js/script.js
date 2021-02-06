606db39a44f61d3a71f469357b11bbb1cdcfef45
Kate <unicornmagic6111@gmail.com>
2021-02-06 19:01:42 +0300

    Made changes only in script.js Objects and Arrays

--- js/script.js
+++ js/script.js
@@ -77,4 +77,99 @@
 // console.log(parseFloat(test));
 // console.log(isNaN(test));
 
+// CALLBACK 
 
+// function learnJS(lang, callback) {
+//    console.log(`Я учу: ${lang}`);
+//    callback();
+// } 
+
+// function done() {
+//    console.log('Я прошел этот урок!');
+// }
+
+// learnJS('Java', done);
+
+
+// ОБЪЕКТ
+
+// const options = {
+//    name: 'test',
+//    width: 1024,
+//    height: 1024,
+//    colors: {
+//       border: 'black',
+//       bg: 'red'
+//    },
+//    makeTest: function(){
+//       console.log("Test");
+//    }
+// };
+
+// options.makeTest();
+
+// // диструктуризация объекта
+
+// const{border, bg} = options.colors;
+// console.log(border);
+
+// // console.log(Object.keys(options).length);
+
+// // console.log(options['colors']['border']);
+
+// delete options.name;
+// console.log(options);
+
+// for (let key in options) {
+//    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// }
+
+
+// перебор всех свойств объекта
+
+// let counter = 0;
+// for (let key in options) {
+//    if (typeof(options[key]) === 'object') {
+//       for (let i in options[key]) {
+//          console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//          counter++;
+//       }
+//    } else {
+//    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//    counter++;
+//    }
+// }
+// console.log(counter);
+
+// МАССИВЫ 
+
+const arr = [2, 3, 6, 8, 10];
+
+//если добавить элемент в определенный индекс, то остальные элементы создадутс и будут пустые
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr); 
+
+// arr.pop();
+// arr.push(10);
+
+// Перебор элементов массива
+// for(let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//    console.log(value);
+// }
+
+//  arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+//  });
+
+//разрезает строку по указанному разделителю и возвращает массив
+const str = "jhgsdg, ksdjs, ssk";
+const products = str.split(", ");
+//join выводит массив в виде строки с указанным разделителем
+pr
+
+console.log(products.join('; '));
